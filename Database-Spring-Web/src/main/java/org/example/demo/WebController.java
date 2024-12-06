e0112ccf32d5ac408dd46f12df2bfa0b3f702525
Alan <ddouble.cn@gmail.com>
2024-12-06 12:10:38 +0100

    Add Database-Spring-Web sample

---
+++ Database-Spring-Web/src/main/java/org/example/demo/WebController.java
@@ -0,0 +1,20 @@
+package org.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class WebController {
+
+    @GetMapping("/database-spring-web/querystring")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World")
+            String name,
+            Model templateData
+    ) {
+        templateData.addAttribute("name", name);
+        return "querystring";
+    }
+
+}
